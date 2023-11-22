2753d149cc56c23a66b75b61ec2377fd636bb658
peshkes <peshkesv@gmail.com>
2023-11-22 11:51:54 +0200

    Added route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router";
 import NavigationRow from "./general/components/navigation/NavigationRow";
 import NewsletterSubscribe from "./general/components/newsletter_subscribe/NewsletterSubscribe";
 import Footer from "./general/components/footer/Footer";
+import ShopPage from "./modules/shop-page/presentation/pages/ShopPage";
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
             }}>
                 <Routes>
                     <Route path="/" element={<WeatherPage/>}/>
+                    <Route path="shop" element={<ShopPage/>}/>
                     <Route element={<WeatherPage/>}/>{/*default route*/}
                 </Routes>
             </main>
