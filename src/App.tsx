044c8d150aa19d3e2b522ff206fd0f7f1ff1617d
97995 <dioujikov@gmail.com>
2023-11-24 10:22:48 +0200

    Added homepage alfa version with blocks:Navigation, Newsletter, Footer, Slider section, Banner grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,21 @@ import {Route, Routes} from "react-router";
 import NavigationRow from "./general/components/navigation/NavigationRow";
 import NewsletterSubscribe from "./general/components/newsletter_subscribe/NewsletterSubscribe";
 import Footer from "./general/components/footer/Footer";
+import HomePage from "./modules/home_page/presentation/page/HomePage";
 
 function App() {
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100vh"
-        }}>
+        <div className={"wrapper"}>
+        {/*<div>*/}
             <NavigationRow/>
-            <main style={{
-                flex: 1
-            }}>
-                <Routes>
-                    <Route path="/" element={<WeatherPage/>}/>
-                    <Route element={<WeatherPage/>}/>{/*default route*/}
-                </Routes>
-            </main>
+            {/*<main style={{*/}
+            {/*    flex: 1*/}
+            {/*}}>*/}
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route element={<WeatherPage/>}/>{/*default route*/}
+            </Routes>
+            {/*</main>*/}
             <NewsletterSubscribe/>
             <Footer/>
         </div>
