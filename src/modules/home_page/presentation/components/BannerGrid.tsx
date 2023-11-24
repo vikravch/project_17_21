d54720fadeb7704bf53e6fae602f0e69bdcadee7
vikravch <vitaly.kravchyk@gmail.com>
2023-11-24 09:46:21 +0100

    Added svg component

--- src/modules/home_page/presentation/components/BannerGrid.tsx
+++ src/modules/home_page/presentation/components/BannerGrid.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../page/HomePage.css"
+import Arrow from "../icons/Arrow";
 
 const BannerGrid = () => {
     return (
@@ -9,14 +10,7 @@ const BannerGrid = () => {
                     <h6 className={'banner_grid_title'}>Living Room</h6>
                     <div className={'shop_now_link'}>
                         <button className={'shop_now_btn'}>Shop Now</button>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                            <path d="M3.33331 8H12.6666" stroke="#141718" stroke-width="1.25" stroke-linecap="round"
-                                  stroke-linejoin="round"/>
-                            <path d="M8.66669 12L12.6667 8" stroke="#141718" stroke-width="1.25" stroke-linecap="round"
-                                  stroke-linejoin="round"/>
-                            <path d="M8.66669 4L12.6667 8" stroke="#141718" stroke-width="1.25" stroke-linecap="round"
-                                  stroke-linejoin="round"/>
-                        </svg>
+                        <Arrow color="#141718" />
                     </div>
                 </div>
             </div>
