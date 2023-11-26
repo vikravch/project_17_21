e409ac60aeae06c48ab73700fc6d8a4c0ccde0d6
Larisa Becket <lbikteeva@gmail.com>
2023-11-26 15:00:29 +0200

    Added Articles section

---
+++ src/modules/home_page/presentation/components/Articles/ArticleCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "../../page/HomePage.css";
+import "./ArticleCard.css";
+import Arrow from "../../icons/Arrow";
+
+interface ArticleProps {
+    img: string;
+    title: string;
+    text: string;
+    onClick: () => void;
+}
+
+const ArticleCard= (props: ArticleProps) =>
+{
+    return (
+        <div className={'wrapper'}>
+            <div className={'articles_image'} style={{background: `url(${props.img}) lightgray 50% / cover no-repeat`}}>
+
+            </div>
+            <div className={'img-info'}>
+            <h3 className={'article_title'}>{props.title}</h3>
+            <button onClick={props.onClick} className={'readMoreButton'}>
+                Read more <Arrow color='#141718'/>
+            </button>
+            </div>
+        </div>
+    );
+};
+
+export default ArticleCard;
+
