b14d84ce445de26ab51229ac78d76447c7ace70d
iamogir <makedonski.dragon@gmail.com>
2023-11-29 19:16:27 +0300

    fixed css sorting desktop3 +

--- src/modules/shop-page/presentation/components/sorting/Sorting.tsx
+++ src/modules/shop-page/presentation/components/sorting/Sorting.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import {sort} from "../../../domain/model/filterConst";
 import ViewSelector from "../view-selector/ViewSelector";
 import style from './sorting.module.css';
+import {Columns} from "../../redux/types";
 
 interface Props {
+    columns: Columns;
     openCloseMenuHandler: (event: any) => void;
     chooseSortOrFiltration: (event: any) => void;
 
 }
 
-const Sorting = ({openCloseMenuHandler, chooseSortOrFiltration}: Props) => {
+const Sorting = ({columns, openCloseMenuHandler, chooseSortOrFiltration}: Props) => {
     return (
-        <div className={style.sortBlock}>
+        <div className={`${style.sortBlock} ${columns.countDesktop === 3 && style.sortBlockDisplay3}`}>
             <div className={style.sortSelect}>
                 <input type={'hidden'} name={'sort'}/>
-                <div className={`${style.sortHead} listenerHead`} onClick={openCloseMenuHandler}>Sort by</div>
+                <div className={`${style.sortHead} ${columns.countDesktop === 3 && style.sortHeaDisplay3} listenerHead`} onClick={openCloseMenuHandler}>Sort by</div>
                 <ul className={`${style.sortList} listener`}>
                     <li>Sort by</li>
                     {sort.map(item => {
