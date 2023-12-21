5496a86fc70f256f63755d0484856046a213c174
Sveta <svetlana.denisyuk@gmail.com>
2023-12-21 14:02:37 +0200

    added react-hook-form

--- src/modules/account/presentation/components/accountDetailsForm/AccountDetailsForm.tsx
+++ src/modules/account/presentation/components/accountDetailsForm/AccountDetailsForm.tsx
@@ -1,42 +1,92 @@
 import React from 'react';
 import styles from "../accountDetailsForm/accountDetailsForm.module.css";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {FormDataAccount} from "../../mock-api/data";
 
 
 const AccountDetailsForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+        watch
+    } = useForm<FormDataAccount>({
+        mode: 'onTouched',
+    });
+    const newPassword = watch('newPassword');
+
+
+    const onSubmit: SubmitHandler<FormDataAccount> = data => {
+        console.log(data)
+    }
     return (
         <section className={styles.accountDetailsForm}>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
                 <h2 className={styles.titleForm}>Account Details</h2>
                 <fieldset className={styles.inputContainer}>
                     <label>FIRST NAME*</label>
-                    <input placeholder="First name"/>
+                    <input {...register('firstName')}
+                           type={"text"}
+                           id="firstName"
+                           placeholder="First name"/>
                 </fieldset>
                 <fieldset className={styles.inputContainer}>
                     <label>LAST NAME*</label>
-                    <input placeholder="Last name"/>
+                    <input {...register('lastName')}
+                           type="text"
+                           id="lastName"
+                           placeholder="Last name"/>
                 </fieldset>
                 <fieldset className={styles.inputContainer}>
                     <label>DISPLAY NAME*</label>
-                    <input placeholder="Display name"/>
+                    <input placeholder="Display name"
+                           {...register('displayName')}
+                           type="text"
+                           id="displayName"/>
                     <p>This will be how your name will be displayed in the account section and in reviews</p>
                 </fieldset>
                 <fieldset className={styles.inputContainer}>
                     <label>EMAIL*</label>
-                    <input placeholder="Email"/>
+                    <input placeholder="Email"
+                           {...register('email')}
+                           type="email"
+                           id="email"
+                    />
                 </fieldset>
-
                 <h2 className={styles.titleForm}>Password</h2>
                 <fieldset className={styles.inputContainer}>
                     <label>OLD PASSWORD</label>
-                    <input placeholder="Old password"/>
+                    <input placeholder="Old password"
+                           {...register('oldPassword', {
+                               required: newPassword ? 'Old password is required when setting a new password' : false
+                           })}
+                           type="password"
+                           id="oldPassword"
+                    />
+                    {errors.oldPassword && <p>{errors.oldPassword.message}</p>}
                 </fieldset>
                 <fieldset className={styles.inputContainer}>
                     <label>NEW PASSWORD</label>
-                    <input placeholder="New password"/>
+                    <input placeholder="New password"
+                           {...register('newPassword', {
+                               minLength: {
+                                   value: 8,
+                                   message: 'Password must have at least 8 characters'
+                               }
+                           })}
+                           type="password"
+                           id="newPassword"/>
                 </fieldset>
                 <fieldset className={styles.inputContainer}>
                     <label>REPEAT NEW PASSWORD</label>
-                    <input placeholder="Repeat new password"/>
+                    <input placeholder="Repeat new password"
+                           {...register('confirmPassword', {
+                               validate: value =>
+                                   value === newPassword || 'The passwords do not match'
+                           })}
+                           type="password"
+                           id="confirmPassword"/>
+                    {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
                 </fieldset>
                 <button type={"submit"}>Save changes</button>
             </form>
--- src/modules/account/presentation/mock-api/data.ts
+++ src/modules/account/presentation/mock-api/data.ts
@@ -3,86 +3,84 @@ import trayTable from "../../images/trayTable.webp"
 import bambooBasket from "../../images/bambooBasket.webp"
 import pillow from "../../images/pillow.webp"
 
-export type TUserProfile = {
-    firstName: string;
-    lastName: string;
-    avatarUrl: string;
-}
-export type TAddress={
-    firstName: string
-    lastName: string
-    phoneNumber: string
-    address: string
-    picture:string
-}
-export type TItemWishList={
-    id:number
-    name:string
-    color:string
+export type FormDataAccount = {
+    firstName?: string;
+    lastName?: string;
+    displayName?: string;
+    email?: string;
+    oldPassword?: string;
+    newPassword?: string;
+    confirmPassword?: string;
+};
+
+export type TItemWishList = {
+    id: number
+    name: string
+    color: string
     price: string;
     picture: string;
 }
-export type TItemOrder={
+export type TItemOrder = {
     id: number
     dateOrder: string
-    status:string
+    status: string
     price: number
 }
 
-export const dataOrders: TItemOrder[]=[
+export const dataOrders: TItemOrder[] = [
     {
         id: 3456_768,
         dateOrder: "October 17, 2023",
-        status:"Delivered",
+        status: "Delivered",
         price: 1234
     },
     {
         id: 3456_768,
         dateOrder: "October 11, 2023",
-        status:"Delivered",
+        status: "Delivered",
         price: 345
     },
     {
         id: 3456_768,
         dateOrder: "August 24, 2023",
-        status:"Delivered",
+        status: "Delivered",
         price: 2345
     },
     {
         id: 3456_768,
         dateOrder: "August 12, 2023",
-        status:"Delivered",
+        status: "Delivered",
         price: 845
     },
 
 ]
-export const dataWishList:TItemWishList[]=[
+export const dataWishList: TItemWishList[] = [
     {
-        id:1,
-        name:"Tray Table",
-        color:"Black",
-        price:"$19.19",
+        id: 1,
+        name: "Tray Table",
+        color: "Black",
+        price: "$19.19",
         picture: trayTable
     },
     {
-        id:2,
-        name:"Sofa",
-        color:"Beige",
-        price:"$345",
+        id: 2,
+        name: "Sofa",
+        color: "Beige",
+        price: "$345",
         picture: sofa
     },
     {
-        id:3,
-        name:"Bamboo Basket",
-        color:"Beige",
-        price:"$8.80",
+        id: 3,
+        name: "Bamboo Basket",
+        color: "Beige",
+        price: "$8.80",
         picture: bambooBasket
     },
     {
-        id:4,
-        name:"Pillow",
-        color:"Beige",
-        price:"$8.80",
+        id: 4,
+        name: "Pillow",
+        color: "Beige",
+        price: "$8.80",
         picture: pillow
     }
 ]
