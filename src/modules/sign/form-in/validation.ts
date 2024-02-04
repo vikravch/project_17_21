e23eab80c6183c75f7b5fea618af05cbcafd2338
Sergay35 <122868413+Sergay35@users.noreply.github.com>
2024-02-04 20:47:46 +0200

    Add files via upload

--- src/modules/sign/form-in/validation.ts
+++ src/modules/sign/form-in/validation.ts
@@ -1,11 +1,14 @@
 const REQUIRED_FIELD = 'The fields are required!'
+
 export const loginValidation = {
     required:REQUIRED_FIELD,
     validate: (value: string) => {
 
-        if (value.match(/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i)){
+        if (!value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
             return 'Email entered incorrectly'
         }
+        if (value.length>50) {
+            return 'Mail too long';}
    return true;
     }
 }
@@ -13,9 +16,11 @@ export const passwordValidation = {
    required: REQUIRED_FIELD,
    validate: (value: string) => {
      
-    if(value.length <6) {
-        return 'Password must be longer than 6 symbols!'
-    }
+    if(value.length <8) {
+        return 'Password must be longer than 8 symbols!'
+    } 
+    if (!value.match(/[a-z]/) || !value.match(/[A-Z]/) || !value.match(/[0-9]/)) {
+        return 'Password must be have more than one uppercase letter more than  one number and  more than one lowercase letter '}
     return true;
    }
 }
